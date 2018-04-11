43db8377e8ab189dd1656642d13711cf8c503fc4
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-04-10 23:17:48 -0400

    Documented some code

--- src/fif/core.clj
+++ src/fif/core.clj
@@ -8,38 +8,67 @@
 (def get-ret stack/get-ret)
 
 
-(def ^:dynamic *default-stack*
+(def ^:dynamic
+  *default-stack*
+  "Default Stack Machine, containing all of the standard
+  libraries. Can be used with `with-stack` in order to rebind for
+  `eval`, `seval` and `reval`"
   (-> (stack/new-stack-machine)
       import-stdlib))
 
 
-(defmacro with-stack [sm & body]
+(defmacro with-stack
+  "Used to rebind the `*default-stack*` for use with `eval`, `seval` and
+  `reval`"
+  [sm & body]
   `(binding [*default-stack* ~sm]
      ~@body))
 
 
-(defn eval-fn [args]
+(defn eval-fn
+  "Evaluate the sequence of args within the fif stackmachine, and return
+  the stackmachine as a result."
+  [args]
   (-> *default-stack*
       (stack/eval-fn args)))
 
 
-(defmacro eval [& body]
+(defmacro eval
+   "Evaluate the values provided in `body` within the fif
+   stackmachine, and return the stackmachine"
+   [& body]
   `(eval-fn (quote ~body)))
 
 
-(defmacro seval [& body]
+(defmacro seval
+  "Evaluate the values provided in `body`, and return the main stack
+  after completion.
+
+  Notes:
+
+  - You will likely want to use fif.core/reval, since it returns the
+  main stack in a more pleasing orientation."
+  [& body]
   `(-> (eval-fn (quote ~body)) stack/get-stack))
 
 
-(defmacro reval [& body]
+(defmacro reval
+  "Evaluate the values provided in `body` within the fif stackmachine,
+  and return the main stack as a result."
+  [& body]
   `(-> (eval-fn (quote ~body)) stack/get-stack reverse))
 
 
-(defn eval-string [s]
+(defn eval-string
+  "Evaluates the given string as a stream of EDN values within the fif
+  stackmachine, and returns the stackmachine after evaluation."
+  [s]
   (-> *default-stack* (stack/eval-string s)))
 
 
-(defmacro dbg-eval [opts & body]
+(defmacro dbg-eval
+  "Debugging tool. By default enables the step inhibitor"
+  [opts & body]
   `(let [step-max# (get ~opts :step-max 100)]
      (-> *default-stack*
          (stack/set-step-max step-max#)
@@ -145,7 +174,7 @@
    macro ?do
      over over >
      if
-       _$ do $_
+       _$ inc do $_
      else
        _$ do leave $_
      then
@@ -158,7 +187,7 @@
      "hhh!" . cr
    endfn
 
-   0 yeaa!)
+   2 yeaa!)
 
 
 #_(->> (dbg-eval {:step-max 50}
@@ -230,14 +259,14 @@
    dup 1 > if dup dec factorial * then
    endfn
    
-   5 factorial
-   >r)
+   5 factorial)
+
 
 
 #_(reval fn add2 2 + endfn 2 add2 >r)
 
 
-#_(eval
+#_(reval
    fn cond1 if true else false then endfn
 
    0 cond1 .s)
