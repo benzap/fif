0226e197290f47393305b788d49622c07af72624
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-04-18 19:42:34 -0400

    Implemented Locally scoped variables

--- src/fif/stdlib/variable.clj
+++ src/fif/stdlib/variable.clj
@@ -39,15 +39,18 @@
   (mode/exit-mode sm))
 
 
+(defmulti variable-mode mode/mode-dispatch-fn)
+
+
+;; Global Variable Implementation
+
+
 (defn start-global-variable
   "Puts the stack machine into variable-mode"
   [sm]
   (-> sm
       (enter-variable-mode {:op ::global-variable :op-state ::get-symbol})
       stack/dequeue-code))
-
-
-(defmulti variable-mode mode/mode-dispatch-fn)
 
 
 (defmethod variable-mode
@@ -78,35 +81,48 @@
         stack/dequeue-code)))
 
 
-(defn variable-mode2
-  "Variable mode is used to define a variable, which can then be
-  manipulated with two provided methods."
-  [sm]
-  (let [arg (-> sm stack/get-code first)]
-    (cond
-      (symbol? arg)
-      (-> sm
-          (stack/push-stack arg)
-          stack/dequeue-code)
-      :else
-      (let [vname (-> sm stack/get-stack peek)]
-        (-> sm
-            (stack.words/set-global-word vname (wrap-global-variable arg))
-            stack/pop-stack
-            (stack/pop-flag)
-            stack/dequeue-code)))))
-
-
-
+;; Local Variable Implementation
 
 
 (defn start-local-variable
-  "Puts the stack machine into variable-mode for local variables."
-  [sm])
+  "Puts the stack machine into variable-mode to create a local
+  variable."
+  [sm]
+  (-> sm
+      (enter-variable-mode {:op ::local-variable :op-state ::get-symbol})
+      stack/dequeue-code))
+
+
+(defmethod variable-mode
+  {:op ::local-variable :op-state ::get-symbol}
+  [sm]
+  (let [arg (-> sm stack/get-code first)]
+    (if (symbol? arg)
+      (-> sm
+          (stack/push-stack arg)
+          (mode/update-state assoc :op-state ::set)
+          stack/dequeue-code)
+
+      ;; TODO: throw stack error
+      (-> sm
+          (stack/push-stack "Def: Undefined Behaviour!")
+          (stack/halt)))))
+
+
+(defmethod variable-mode
+  {:op ::local-variable :op-state ::set}
+  [sm]
+  (let [val (-> sm stack/get-code first)
+        sym (-> sm stack/get-stack peek)]
+    (-> sm
+        (stack.words/set-word sym (wrap-local-variable val))
+        stack/pop-stack
+        exit-variable-mode
+        stack/dequeue-code)))
 
 
 (defn setg
-  "Word function used to set a variable to a provided value"
+  "Word function used to set a global variable to a provided value"
   [sm]
   (let [[val sym] (stack/get-stack sm)]
     ;; TODO: check if variable exists
@@ -117,10 +133,24 @@
         stack/dequeue-code)))
 
 
+(defn setl
+  "Word function used to set a local variable to a provided value"
+  [sm]
+  (let [[val sym] (stack/get-stack sm)]
+    ;; TODO: check if variable exists
+    (-> sm
+        stack/pop-stack
+        stack/pop-stack
+        (stack.words/set-word sym (wrap-global-variable val))
+        stack/dequeue-code)))
+
+
 (defn import-stdlib-variable-mode
   "Stack Machine Import for variable-mode"
   [sm]
   (-> sm
       (stack/set-word arg-global-var-token start-global-variable)
       (stack/set-word 'setg setg)
+      (stack/set-word arg-local-var-token start-local-variable)
+      (stack/set-word 'setl setl)
       (stack/set-mode variable-mode-flag variable-mode)))
--- test/fif/stdlib/variable_test.clj
+++ test/fif/stdlib/variable_test.clj
@@ -13,14 +13,6 @@
    => '(20)))
 
 
-(deftest test-variable-creation
-  (are-eq*
-
-   (teval def x 20 x)
-   
-   => '(20)))
-
-
 (deftest test-variable-set
   (are-eq*
    (teval
@@ -28,5 +20,17 @@
     def y 2
 
     *x 10 setg x)
+  
+   => '(10)))
+
+
+(deftest test-variable-let
+  (are-eq*
+   (teval
+    let x 1
+    let y 2
+
+    *x 10 setl x)
+    
   
    => '(10)))
