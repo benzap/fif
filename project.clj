37a3795178a03aeaa1507c0263caaa5e5a58085f
Benjamin Zaporzan <benzaporzan@gmail.com>
2018-04-19 01:26:14 -0400

    Preparing clojure tests for clojurescript

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
-                 [org.clojure/tools.reader "1.2.1"]]
+                 [org.clojure/tools.reader "1.2.2"]]
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-ancient "0.6.15"]]
+            [lein-ancient "0.6.15"]
+            [lein-doo "0.1.10"]]
   ;;:hooks [leiningen.cljsbuild]
-  :repositories [["clojars" {:sign-releases true}]]
+  :repositories [["clojars" {:sign-releases false}]]
   :cljsbuild {:builds {:dev
                        {:source-paths ["src"]
                         :compiler {:output-dir "resources/public/js/compiled/out"
@@ -21,4 +22,14 @@
                        {:source-paths ["src"]
                         :compiler {:output-to "resources/public/js/compiled/fif.min.js"
                                    :optimizations :advanced
-                                   :pretty-print false}}}})
+                                   :pretty-print false}}
+                       :test
+                       {:id "test"
+                        :source-paths ["src" "test"]
+                        :compiler {:output-to "resources/public/js/compiled/test/test-runner.js"
+                                   :output-dir "resources/public/js/compiled/test/out"
+                                   :main fif.test-runner
+                                   :target :nodejs
+                                   :optimizations :none}}}}
+   :doo {:build "test"
+         :alias {:default [:node]}})
---
+++ test/fif/test_runner.cljs
@@ -0,0 +1,9 @@
+(ns fif.test-runner
+  (:require
+   [doo.runner :refer-macros [doo-tests doo-all-tests]]
+
+   ;; Test Cases
+   [fif.stdlib.ops-test]))
+
+
+(doo-all-tests)
