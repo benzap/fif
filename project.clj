a8b0bf5292e8809a347e3d435bfa8f3bb0f20b83
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-03-31 17:16:18 -0400

    Updated Readme, updated project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fif "1.3.0"
+(defproject fif-lang/fif "1.3.0"
   :description "Stack-based Programming in Clojure(script)"
   :url "http://github.com/benzap/fif"
   :license {:name "Eclipse Public License"
